0d0ccf36b18c5ab091d3a8f139ead9f21ac8da62
AdmAlexandreMattos <admalexandremattos@gmail.com>
2025-04-29 16:51:34 -0300

    Card de colaborador
    Criando o Card Colaborador para ser exibido dentro do time

---
+++ src/componentes/Colaborador/Colaborador.js
@@ -0,0 +1,18 @@
+import "./Colaborador.css";
+
+export const Colaborador = (props) => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img
+          src="https://github.com/AdmAlexandreMattos.png"
+          alt="Alexandre Mattos de Castro"
+        />
+      </div>
+      <div className="rodape">
+        <h4>Alexandre Mattos</h4>
+        <h5>Front-End</h5>
+      </div>
+    </div>
+  );
+};
