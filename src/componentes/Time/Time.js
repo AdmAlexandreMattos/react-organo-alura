d31ed23db3a84406ad5aee253a02123b40b2132d
AdmAlexandreMattos <admalexandremattos@gmail.com>
2025-04-29 14:57:59 -0300

    Elevando a lista de times
    Fazendo com que o componente Time receba parâmetros, utilizando as cores corretas de acordo com o time e simplificando o código fazendo o uso do map

--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -2,8 +2,8 @@ import "./Time.css";
 
 export const Time = (props) => {
   return (
-    <section className="time">
-      <h3>{props.nomeTime}</h3>
+    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nomeTime}</h3>
     </section>
   );
 };
